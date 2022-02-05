78f32fc1cff7784268d1da4c69bb3e72ca53d070
Stephan Göppentin <dedda102@gmail.com>
2022-02-05 12:34:25 +0100

    json decoders for first types of item resources

--- web/src/Data.elm
+++ web/src/Data.elm
@@ -1,6 +1,7 @@
 module Data exposing (..)
 
-import Json.Decode as Decode exposing (Decoder, decodeString, int, nullable, string)
+import Json.Decode as Decode exposing (..)
+import Json.Decode.Field as Field
 import Json.Decode.Pipeline exposing (required, optional)
 
 type alias Item =
@@ -28,3 +29,40 @@ statsDecoder =
         |> required "item_count" int
         |> required "aggregated_votes_count" int
         |> required "unaggregated_votes_count" int
+
+type ItemResource
+    = Image ImageResource
+    | Text String
+    | Audio AudioResource
+
+type alias ImageResource =
+    { url : String }
+
+type alias AudioResource =
+    { url : String }
+
+itemResourceDecoder : Decoder ItemResource
+itemResourceDecoder =
+    Field.require "type" Decode.string
+    (\type_ -> case type_ of
+                "image" ->
+                    Field.require "data" imageResourceDecoder
+                        (\data -> Image data |> Decode.succeed)
+                "text" ->
+                    Field.require "data" string
+                        (\data -> Text data |> Decode.succeed)
+                "audio" ->
+                    Field.require "data" audioResourceDecoder
+                        (\data -> Audio data |> Decode.succeed)
+                _ -> Decode.fail "No type given"
+    )
+
+imageResourceDecoder : Decoder ImageResource
+imageResourceDecoder =
+    Decode.succeed ImageResource
+        |> required "url" string
+
+audioResourceDecoder : Decoder AudioResource
+audioResourceDecoder =
+    Decode.succeed AudioResource
+        |> required "url" string
